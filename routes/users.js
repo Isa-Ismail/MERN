f5f1a1546f290cdaa2ce6ea6c6edb0c302e6862b
Fahim_tech_support <fahimismail75.scc@gmail.com>
2021-05-23 13:50:56 +0600

    lol

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const {check, validationResult} = require('express-validator')
 const router = express.Router()
+const User = require('../models/user')
+const gravatar = require('gravatar')
 
 //@route    Post api/users
 //@desc     Register user
@@ -11,11 +13,30 @@ router.post('/', [
     check('email','E-mail is required,').isEmail(),
     check('password', 'password is required (at least 6 digits)').isLength({ min:6 })
 ],
-(req, res) => {
+async (req, res) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
       }
+    
+    const {name, email, password} = req.body
+
+    try{
+    //if users exists
+    let user = await User.findOne({ email })
+
+    if(user){
+        res.status(400).json({ errors: [{msg:'User already exists'}]})
+    }
+    //get user's gravatar
+
+    //encrypt password
+
+    //return jsonwebtoken
+    }catch(err){
+        console.log(err.message)
+        res.status(500)
+    }
 
     console.log(req.body)
     res.send(req.body)
