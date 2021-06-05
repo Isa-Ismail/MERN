deb351fc000d828e1e2af66fac6f15e95d5bd742
Fahim_tech_support <fahimismail75.scc@gmail.com>
2021-06-05 13:34:15 +0600

    italia font

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const config = require('config')
+
 //@route    Post api/users
 //@desc     Register user
 //@access   Public
