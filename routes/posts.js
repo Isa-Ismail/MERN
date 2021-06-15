5442fda94f6273153c0b22ec7cc31d0b859dcf53
Fahim_tech_support <fahimismail75.scc@gmail.com>
2021-06-15 13:50:37 +0600

    'post_route'

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,29 @@
 const express = require('express')
-
+const {check, validationResult} = require('express-validator')
 const router = express.Router()
+const User = require('../models/User')
+const config = require('config')
+const auth = require('../middleware/auth')
 
 //@route    GET api/posts
 //@desc     Test route
 //@access   Public
 
-router.get('/',(req, res) => res.send(`posts api`))
+router.get('/', [
+    check('text', 'Text is required').notEmpty()
+], auth, (req, res) => {
+    
+    const errors = validationResult(req)
+    
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() })
+    }
+    try {
+        
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send(`Server Error`)
+    }
+})
 
 module.exports = router
