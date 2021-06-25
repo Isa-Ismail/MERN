39a0272535c996b9ea9d0abaa7e178a37cbc21b9
Fahim_tech_support <fahimismail75.scc@gmail.com>
2021-06-26 02:18:10 +0600

    'space'

--- Client/src/index.js
+++ Client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
-import { BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import reportWebVitals from "./reportWebVitals";
 import All from './Utils/Resource/Reducers'
 import './index.css'
