01d8065495d18f2e8a671750a8ecbc89281addfc
Fahim_tech_support <fahimismail75.scc@gmail.com>
2021-08-10 23:22:22 +0600

    'edu-xp-action'

--- Client/src/Project Mern/actions/profile.js
+++ Client/src/Project Mern/actions/profile.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
 import setAlert from './alert'
 
-import { PROFILE_ERROR, GET_PROFILE } from './types'
+import { PROFILE_ERROR, GET_PROFILE, UPDATE_PROFILE } from './types'
 
 // get current user profile
-
 export const getCurrentProfile = () => async dispatch => {
 
     try {
@@ -25,6 +24,7 @@ export const getCurrentProfile = () => async dispatch => {
     }
 }
 
+// Creates or updates profile
 export const createProfile = (formData, edit = false) => async dispatch => {
     try {
         const config = {
@@ -55,3 +55,56 @@ export const createProfile = (formData, edit = false) => async dispatch => {
         })
     }
 }
+
+// Add Experience
+export const addExperience = (formData) => async (dispatch) => {
+    try {
+      const res = await axios.put('/profile/experience', formData);
+  
+      dispatch({
+        type: UPDATE_PROFILE,
+        payload: res.data
+      });
+  
+      dispatch(setAlert('Experience Added', 'success'));
+  
+    } catch (err) {
+      const errors = err.response.data.errors;
+  
+      if (errors) {
+        errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+      }
+  
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  };
+  
+  // Add Education
+  export const addEducation = (formData) => async (dispatch) => {
+    try {
+      const res = await axios.put('/profile/education', formData);
+  
+      dispatch({
+        type: UPDATE_PROFILE,
+        payload: res.data
+      });
+  
+      dispatch(setAlert('Education Added', 'success'));
+
+    } catch (err) {
+      const errors = err.response.data.errors;
+  
+      if (errors) {
+        errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+      }
+  
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  };
+  
